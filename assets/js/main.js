20be71ab144e9242728468e11469bd8e539f0f45
biaccshop <biaccshopinfo@gmail.com>
2023-12-18 02:45:07 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
+
+
+// Scroll Reveal
+ScrollReveal({
+  reset: false,
+  distance: '200px',
+  duration: 750,
+  delay: 200
+});
+ScrollReveal().reveal('.hero .hero-content', { origin: 'top', distance: '25px', delay: 200 });
+ScrollReveal().reveal('.hero .hero-img', { origin: 'bottom', distance: '25px', delay: 200 });
+ScrollReveal().reveal('.banner', { origin: 'bottom', distance: '0px', delay: 300 });
+
+
+
 // Navbar anchor + hiding # in URL
 $('a[href^="#"]').on('click', function (event) {
   var target = $(this.getAttribute('href'));
@@ -28,39 +60,6 @@ $(document).ready(function () {
       $("#btn-blackbar").removeClass("active");
       $("#blackbarbutton-icon").removeClass("fa-xmark").addClass("fa-bars");
       menuOpen = false;
-    }
-  });
-});
-
-
-
-// Modal section
-$(document).ready(function () {
-  var modal = $(".modal");
-  var ModalOpened = false;
-
-  $(window).scroll(function () {
-    var modalStarting = document.querySelector('#products-content');
-    var rect = modalStarting.getBoundingClientRect();
-    if (ModalOpened == false) {
-      if (rect.top <= window.innerHeight && rect.bottom >= 0 && !ModalOpened) {
-        modal.css("display", "block");
-        ModalOpened = true;
-      }
-    }
-  });
-
-  modal.on("click", ".close", function () {
-    modal.css("display", "none");
-  });
-
-  modal.on("click", "a", function () {
-    modal.css("display", "none");
-  });
-
-  $(document).on("click", function (event) {
-    if ($(event.target).is(modal)) {
-      modal.css("display", "none");
     }
   });
 });
@@ -142,34 +141,3 @@ rightArrows.forEach(function (arrow) {
     categoryContainer.scrollLeft += containerWidth * 0.8;
   });
 });
-
-
-
-// Scroll Reveal
-ScrollReveal({
-  reset: false,
-  distance: '200px',
-  duration: 750,
-  delay: 200
-});
-ScrollReveal().reveal('.hero .hero-content', { origin: 'top', distance: '25px', delay: 200 });
-ScrollReveal().reveal('.hero .hero-img', { origin: 'bottom', distance: '25px', delay: 200 });
-ScrollReveal().reveal('.banner', { origin: 'bottom', distance: '0px', delay: 300 });
-
-
-// Disable right-click
-document.addEventListener('contextmenu', (e) => e.preventDefault());
-function ctrlShiftKey(e, keyCode) {
-  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
-// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-document.onkeydown = (e) => {
-  if (
-    event.keyCode === 123 ||
-    ctrlShiftKey(e, 'I') ||
-    ctrlShiftKey(e, 'J') ||
-    ctrlShiftKey(e, 'C') ||
-    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-  )
-    return false;
-};
