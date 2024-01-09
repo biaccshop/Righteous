e42de3e4e0766ac94b063dc4f98697760b070f85
biaccshop <biaccshopinfo@gmail.com>
2024-01-09 03:35:55 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,4 @@
-// Disable right-click
-document.addEventListener('contextmenu', (e) => e.preventDefault());
-function ctrlShiftKey(e, keyCode) {
-    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
-// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-document.onkeydown = (e) => {
-    if (
-        event.keyCode === 123 ||
-        ctrlShiftKey(e, 'I') ||
-        ctrlShiftKey(e, 'J') ||
-        ctrlShiftKey(e, 'C') ||
-        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-    )
-        return false;
-};
+
 
 
 
