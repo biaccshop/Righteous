84099eb02195d9bf4857c61678975efebbdaa9b3
biaccshop <biaccshopinfo@gmail.com>
2024-01-18 02:36:26 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,9 +25,13 @@ ScrollReveal({
   delay: 200
 });
 
-ScrollReveal().reveal('.hero', { origin: 'top', distance: '25px', delay: 200 });
+ScrollReveal().reveal('.hero-content', { origin: 'left', distance: '100px', delay: 200 });
+ScrollReveal().reveal('.hero-img', { origin: 'right', distance: '100px', delay: 200 });
+ScrollReveal().reveal('.statistics', { origin: 'top', distance: '0px', delay: 600 });
+ScrollReveal().reveal('.banner-div', { origin: 'top', distance: '0px', delay: 600 });
 ScrollReveal().reveal('.feedbacks-title', { origin: 'top', distance: '25px', delay: 200 });
 ScrollReveal().reveal('.feedbacks .slider', { origin: 'top', distance: '0px', delay: 600 });
+ScrollReveal().reveal('.contact', { origin: 'top', distance: '0px', delay: 200 });
 ScrollReveal().reveal('.products', { origin: 'top', distance: '0px', delay: 200 });
 ScrollReveal().reveal('.faq', { origin: 'top', distance: '0px', delay: 200 });
 ScrollReveal().reveal('.tos', { origin: 'top', distance: '0px', delay: 200 });
