f5ca331420c2bffe5c7c6b915b705176f05ffb89
biaccshop <biaccshopinfo@gmail.com>
2024-01-23 07:31:04 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-// Disable right-click
-document.addEventListener('contextmenu', (e) => e.preventDefault());
-function ctrlShiftKey(e, keyCode) {
-    return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
-// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-document.onkeydown = (e) => {
-    if (
-        event.keyCode === 123 ||
-        ctrlShiftKey(e, 'I') ||
-        ctrlShiftKey(e, 'J') ||
-        ctrlShiftKey(e, 'C') ||
-        (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-    )
-        return false;
-};
-
-
-
 // Scroll Reveal
 ScrollReveal({
   reset: false,
@@ -47,27 +28,4 @@ $('a[href^="#"]').on('click', function (event) {
       scrollTop: target.offset().top + 4 * parseFloat(getComputedStyle(document.documentElement).fontSize)
     }, 300);
   }
-});
-
-
-
-// Black Bar toggle
-$(document).ready(function () {
-  var menuOpen = false;
-
-  $("#btn-blackbar, .nav-blackbar-item").click(function () {
-    if (!menuOpen) {
-      $("body").css("overflow", "hidden");
-      $("#black-bar").slideDown();
-      $("#btn-blackbar").addClass("active");
-      $("#blackbarbutton-icon").removeClass("fa-bars").addClass("fa-xmark");
-      menuOpen = true;
-    } else {
-      $("body").css("overflow", "auto");
-      $("#black-bar").slideUp();
-      $("#btn-blackbar").removeClass("active");
-      $("#blackbarbutton-icon").removeClass("fa-xmark").addClass("fa-bars");
-      menuOpen = false;
-    }
-  });
 });
