a27f32186a70008d34711e54a4c86183e00d04f2
biaccshop <biaccshopinfo@gmail.com>
2024-01-23 11:11:03 +0300

    New Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,40 @@
+// Disable right-click
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+document.onkeydown = (e) => {
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
+
 // Scroll Reveal
 ScrollReveal({
   reset: false,
   distance: '400px',
-  duration: 750,
+  duration: 1000,
   delay: 200
 });
 
-ScrollReveal().reveal('.hero-content', { origin: 'left', distance: '100px', delay: 200 });
-ScrollReveal().reveal('.hero-img', { origin: 'right', distance: '100px', delay: 200 });
+ScrollReveal().reveal('.hero-content', { origin: 'left', distance: '0px', delay: 200 });
+ScrollReveal().reveal('.hero-img', { origin: 'right', distance: '0px', delay: 200 });
 ScrollReveal().reveal('.statistics', { origin: 'top', distance: '0px', delay: 600 });
-ScrollReveal().reveal('.banner-div', { origin: 'top', distance: '0px', delay: 600 });
-ScrollReveal().reveal('.feedbacks-title', { origin: 'top', distance: '25px', delay: 200 });
-ScrollReveal().reveal('.feedbacks .slider', { origin: 'top', distance: '0px', delay: 600 });
-ScrollReveal().reveal('.contact', { origin: 'top', distance: '0px', delay: 200 });
-ScrollReveal().reveal('.products', { origin: 'top', distance: '0px', delay: 200 });
-ScrollReveal().reveal('.faq', { origin: 'top', distance: '0px', delay: 200 });
-ScrollReveal().reveal('.tos', { origin: 'top', distance: '0px', delay: 200 });
-
-
+ScrollReveal().reveal('.about-content', { origin: 'left', distance: '200px', delay: 200 });
+ScrollReveal().reveal('.about-img', { origin: 'right', distance: '200px', delay: 200 });
+ScrollReveal().reveal('.about2-content', { origin: 'left', distance: '200px', delay: 200 });
+ScrollReveal().reveal('.about2-img', { origin: 'right', distance: '200px', delay: 200 });
+ScrollReveal().reveal('.about3-content', { origin: 'left', distance: '200px', delay: 200 });
+ScrollReveal().reveal('.about3-img', { origin: 'right', distance: '200px', delay: 200 });
+ScrollReveal().reveal('.feedbacks-title', { origin: 'left', distance: '0px', delay: 200 });
+ScrollReveal().reveal('.feedbacks-content', { origin: 'top', distance: '0px', delay: 600 });
+ScrollReveal().reveal('.contact-title', { origin: 'left', distance: '0px', delay: 200 });
 
 // Navbar anchor + hiding # in URL
 $('a[href^="#"]').on('click', function (event) {
